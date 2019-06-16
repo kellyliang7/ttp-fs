3029131f0f5cc034ea22814fa258b3b0f3fdd416
Kelly <kellyliang95@gmail.com>
2019-06-15 22:05:03 -0400

    Set state and render auth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,10 +9,44 @@ class App extends React.Component {
     this.state = {}
   }
 
+  isLoggedIn = () => {
+    axios.get('/users/isLoggedIn')
+      .then(res => {
+        const user = res.data;
+        this.setLoggedInUser(user);
+      })
+      .catch(err => {
+        console.log("Error =>", err);
+      })
+  }
+
+  componentDidMount(){
+    this.isLoggedIn();
+  }
+
+  setLoggedInUser = (user) => {
+    this.setState({
+      user: user,
+      userLoggedIn: true
+    })
+  }
+
+  renderAuth = (routeProps) => {
+    return (
+      <Auth 
+        setLoggedInUser={this.setLoggedInUser}
+        {...routeProps}
+      />
+    )
+  }
+
   render() {
     return(
       <React.Fragment>
         <NavBar /> 
+        <Switch>
+
+        </Switch>
       </React.Fragment>
     )
   }
