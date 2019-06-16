c088ab9b373b1628decaf5a0876648b09645297f
Kelly <kellyliang95@gmail.com>
2019-06-16 17:56:24 -0400

    Validate quantity of shares

--- frontend/src/Components/Portfolio/PurchaseForm.js
+++ frontend/src/Components/Portfolio/PurchaseForm.js
@@ -8,12 +8,21 @@ class PurchaseForm extends React.Component {
       balance: 0,
       ticker: "",
       quantity: 0,
-      price: 0
+      stock: {}
     }
   }
   APIUrl = (ticker) => {
     return `https://cloud.iexapis.com/stable/stock/${ticker}/quote?token=pk_6671a714d5da41efbec42097ab376ba2`
   };
+
+  getStockInfo = () => {
+    axios.get(this.APIUrl(this.state.ticker))
+    .then(res => {
+      this.setState({stock: res.data})
+    }).catch(err => {
+      console.log(err)
+    })
+  }
 
   handleChange = (e) => {
     this.setState({[e.target.name]: e.target.value})
@@ -22,11 +31,11 @@ class PurchaseForm extends React.Component {
   handleSubmit = (e) => {
     e.preventDefault();
     axios.post(`http://localhost:3001/transactions/new/`, {
-      users_id: this.props.users.id,
+      users_id: this.props.user.id,
       ticker: this.state.ticker,
       transaction_type: "BUY",
       quantity: this.state.quantity,
-      price: this.state.price
+      price: this.state.stock.latestPrice
     })
       .then(res => {
       console.log(res)
@@ -35,12 +44,27 @@ class PurchaseForm extends React.Component {
     })
   }
 
+  validateQuantity = () => {
+    return Number.isInteger(this.state.quantity) 
+  }
+
+  validateBalance = () => {
+    const balance = this.props.user.balance
+    const quantity = this.state.quantity
+    const price = this.state.stock.latestPrice
+    if(balance >= quantity * price){
+      return true 
+    } else {
+      return false 
+    }
+  }
+
   render() {
     return(
       <>
         <h1>Form</h1>
         <form onSubmit={this.handleSubmit}>
-          <input onChange={this.handleChange} type="text" name="ticker" placeholder="Ticker" value={this.state.ticker} />
+          <input onChange={this.handleChange} type="text" name="ticker" placeholder="Ticker" value={this.state.ticker} onBlur={this.getStockInfo} />
           <input onChange={this.handleChange} type="text" name="quantity" placeholder="Quantity" value={this.state.quantity} />
           <button>Buy</button>
         </form>
