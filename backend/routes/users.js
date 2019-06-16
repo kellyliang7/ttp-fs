3c41da26d40b17f962019fcd01f60d47f2966168
Kelly <kellyliang95@gmail.com>
2019-06-15 20:26:42 -0400

    Create users routes

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const db = require("../db/queries");
+const passport = require("../auth/local");
+const { loginRequired } = require("../auth/helpers");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.post("/new", db.createUser);
+router.post("/login", passport.authenticate("local", {}), db.loginUser);
+router.get("/isLoggedIn", db.isLoggedIn);
+router.post("/logout", loginRequired, db.logoutUser);
 
 module.exports = router;
