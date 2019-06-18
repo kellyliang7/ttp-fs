d68c7b573fdf92d4b620d48896d259ecc7b09c60
Kelly <kellyliang95@gmail.com>
2019-06-17 22:59:14 -0400

    Display and style portfolio items

--- frontend/src/Components/Portfolio/UserPorfolio.js
+++ frontend/src/Components/Portfolio/UserPorfolio.js
@@ -18,18 +18,39 @@ class UserPortfolio extends React.Component {
     console.log('this is props',this.props)
     let users_id = this.props.user.id
     axios.get(`http://localhost:3001/portfolios/${users_id}`).then(res => {
-      this.setState({stocks: res.user_portfolios})
+      this.setState({stocks: res.data.user_portfolios})
+      console.log(res.data)
     })
+    .catch(err => {
+      console.log(err) 
+    })
+  }
+
+  displayPortfolio = () => {
+    let stocks = this.state.stocks
+    let displayPortfolio = stocks.map(stock => {
+      return (
+        <div style={{padding: 10, border: '1px solid #67b890', margin: 10}} key={stock.id}>
+          <p>{stock.ticker_symbol} - {stock.quantity} shares</p>
+        </div>
+      )
+    })
+    return displayPortfolio
   }
 
   render(){
     return(
-      <div>
-        <h2>Portfolio</h2>
+      <>
+      <h2>Portfolio</h2>
+      <div style={{display:'flex', flexFlow:'row nonwrap'}}>
+        <div style={{display:'flex', flexFlow:'column'}}>
+        {this.displayPortfolio()}
+        </div>
         <PurchaseForm 
         user={this.props.user}
         />
       </div>
+      </>
     )
   }
 }
