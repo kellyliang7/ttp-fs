73ff36afcee4b2b6d408d142ebfd413eac864b94
Kelly <kellyliang95@gmail.com>
2019-06-17 22:58:13 -0400

    Fix spelling error on route name

--- backend/routes/userPortfolios.js
+++ backend/routes/userPortfolios.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { getUserPortfolios, createPortfolioItem, getPortfolioItem, updatePortfolioItem } = require('../db/queries/userPortfoliosQueries')
 
 router.get('/:id', getUserPortfolios);
-router.get('/:user_id/:ticker_symbol', getPortfolioItem)
+router.get('/:users_id/:ticker_symbol', getPortfolioItem)
 router.post('/new', createPortfolioItem);
 router.patch('/update', updatePortfolioItem);
 
